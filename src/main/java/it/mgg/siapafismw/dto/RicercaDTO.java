715169596bc0633657fafbfeb9ad6ff490659dfd
BIagio <bia.montesano1983@gmail.com>
2023-10-18 11:11:22 +0200

    Aggiunto context root, aggiunto DTO di ricerca

---
+++ src/main/java/it/mgg/siapafismw/dto/RicercaDTO.java
@@ -0,0 +1,14 @@
+package it.mgg.siapafismw.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RicercaDTO 
+{
+	private String numeroTelefonoFamiliare;
+	private String codiceFiscaleFamiliare;
+}
