3ad2af27c2344944b3e8366a37ca7ecb7f2e42fa
BIagio <bia.montesano1983@gmail.com>
2023-12-13 17:04:26 +0100

    Adeguamenti precedenti alla modifica al salvataggio del familiare

--- src/main/java/it/mgg/siapafismw/utils/SiapAfisMWConstants.java
+++ src/main/java/it/mgg/siapafismw/utils/SiapAfisMWConstants.java
@@ -5,7 +5,7 @@ public class SiapAfisMWConstants
 	public static final String RELAZIONE_PARENTELA_NON_RILEVATA = "NON RILEVATO";
 	public static final Integer DEFAULT_OPERATOR = 10000;
 	public static final String FUNZIONE_COLLOQUIO = "C";
-	public static final String DIR_AUTOR_DIRETTORE = "1";
+	public static final String DIR_AUTOR_DIRETTORE = "0";
 	public static final String TIPO_AUTORIZZAZIONE_PERMANENTE = "P";
 	public static final String STATO_AUTORIZZAZIONE_VALIDA = "V";
 }
